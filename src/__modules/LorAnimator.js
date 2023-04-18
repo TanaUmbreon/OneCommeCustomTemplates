7acd3e0a471275bf3234ec0f178afebb1eee36d8
Tana <finaltruth.shintai@gmail.com>
2023-04-19 00:03:46 +0900

    LorAnimatorクラスにモジュールを分割。

---
+++ src/__modules/LorAnimator.js
@@ -0,0 +1,97 @@
+import { LorAnimationComment } from "../__modules/LorAnimationComment.js";
+
+/** @typedef {import("../__types/onesdk").CommonData} CommonData */
+
+/** @type {number} コメントの次の文字を表示する間隔[ミリ秒] */
+const DELAY_MILLISECONDS = 150;
+
+/**
+ * LoR (Library Of Ruina) 風アニメーションを制御します。
+ */
+export class LorAnimator {
+  /** @type {Map<string, LorAnimationComment>} OneSDK コメントの ID と、それに対応する制御対象の  LoR 風アニメーションコメントを関連付けたマップ */
+  #commentMap;
+  /** @type {LorAnimationComment[]} 制御対象の LoR 風アニメーションコメントのリスト */
+  #commentList;
+  /** @type {LorAnimationChar[]} タイピングアニメーションさせるコメント文字を表示順に列挙させたキュー */
+  #typingQueue;
+  /** @type {number?} タイピングアニメーションの制御に使うタイマー (setInterval) 関数の識別子。値が null の場合はタイマーが動作していない状態を表します */
+  #typingTimerId;
+
+  /** @type {LorAnimationComment[]} 制御対象の LoR 風アニメーションコメントのリスト */
+  get comments() {
+    return this.#commentList;
+  }
+
+  /**
+   * LorCommentAnimator のインスタンスを生成します。
+   */
+  constructor() {
+    this.#commentMap = new Map();
+    this.#commentList = [];
+    this.#typingQueue = [];
+    this.#typingTimerId = null;
+  }
+
+  /**
+   * 指定した OneSDK コメントの配列を新しい制御対象として設定し、状態をリフレッシュします。
+   * 新しい OneSDK コメントが含まれている場合は LorAnimationComment に変換して制御対象に追加します。
+   * 前回に制御対象として指定した OneSDK コメントが含まれている場合は、変換済みの LorAnimationComment をそのまま使用します。
+   * @param {CommonData[]} oneComments 制御対象にする OneSDK コメントの配列。
+   */
+  refresh(oneComments) {
+    this.#stopTypingAnimation();
+
+    const oldMap = this.#commentMap;
+    this.#commentMap = new Map();
+    this.#commentList = [];
+    this.#typingQueue = [];
+
+    oneComments.forEach((oneComment) => {
+      const id = oneComment.data.id;
+      const lorComment = oldMap.get(id) ?? new LorAnimationComment(oneComment);
+      lorComment.refreshContent();
+      this.#commentMap.set(id, lorComment);
+      this.#commentList.push(lorComment);
+      this.#typingQueue.push(...lorComment.characters.filter((c) => !c.hasActivated));
+    });
+
+    this.#startTypingAnimation();
+  }
+
+  /**
+   * タイピングアニメーションを開始します。
+   */
+  #startTypingAnimation() {
+    if (this.#typingTimerId != null) {
+      return;
+    }
+
+    this.#typingTimerId = setInterval(() => {
+      if (this.#typingQueue.length == 0) {
+        this.#stopTypingAnimation();
+        return;
+      }
+
+      const char = this.#typingQueue.shift();
+      if (char == null) {
+        this.#stopTypingAnimation();
+        return;
+      }
+      
+      char.activate();
+    }, DELAY_MILLISECONDS);
+  }
+
+  /**
+   * タイピングアニメーションを停止します。
+   */
+  #stopTypingAnimation() {
+    if (this.#typingTimerId == null) {
+      return;
+    }
+
+    clearInterval(this.#typingTimerId);
+    this.#typingTimerId = null;
+  }
+}
--- src/lor-like-comment/script.js
+++ src/lor-like-comment/script.js
@@ -1,110 +1,15 @@
 /** @typedef {import("../__types/onesdk").BaseComment} BaseComment */
 /** @typedef {import("../__types/onesdk").CommonData} CommonData */
 
-import { LorAnimationComment } from "../__modules/LorAnimationComment.js";
+import { LorAnimator } from "../__modules/LorAnimator.js";
 
 const JSON_PATH = "../../comment.json";
 
 /** @type {number} 画面に表示するコメントの最大数 */
 const LIMIT = 5;
 
-/** @type {number} コメントの次の文字を表示する間隔[ミリ秒] */
-const DELAY_MILLISECONDS = 150;
-
-
 /** @type {number} コメントが完全に表示されてからフェードアウトが始まるまでの時間[ミリ秒] */
 const COMMENT_DISPLAY_MILLISECONDS = 10000;
-
-/**
- * LoR (Library Of Ruina) 風アニメーションを制御します。
- */
-class LorAnimator {
-  /** @type {Map<string, LorAnimationComment>} OneSDK コメントの ID と、それに対応する制御対象の  LoR 風アニメーションコメントを関連付けたマップ */
-  #commentMap;
-  /** @type {LorAnimationComment[]} 制御対象の LoR 風アニメーションコメントのリスト */
-  #commentList;
-  /** @type {LorAnimationChar[]} タイピングアニメーションさせるコメント文字を表示順に列挙させたキュー */
-  #typingQueue;
-  /** @type {number?} タイピングアニメーションの制御に使うタイマー (setInterval) 関数の識別子。値が null の場合はタイマーが動作していない状態を表します */
-  #typingTimerId;
-
-  /** @type {LorAnimationComment[]} 制御対象の LoR 風アニメーションコメントのリスト */
-  get comments() {
-    return this.#commentList;
-  }
-
-  /**
-   * LorCommentAnimator のインスタンスを生成します。
-   */
-  constructor() {
-    this.#commentMap = new Map();
-    this.#commentList = [];
-    this.#typingQueue = [];
-    this.#typingTimerId = null;
-  }
-
-  /**
-   * 指定した OneSDK コメントの配列を新しい制御対象として設定し、状態をリフレッシュします。
-   * 新しい OneSDK コメントが含まれている場合は LorAnimationComment に変換して制御対象に追加します。
-   * 前回に制御対象として指定した OneSDK コメントが含まれている場合は、変換済みの LorAnimationComment をそのまま使用します。
-   * @param {CommonData[]} oneComments 制御対象にする OneSDK コメントの配列。
-   */
-  refresh(oneComments) {
-    this.#stopTypingAnimation();
-
-    const oldMap = this.#commentMap;
-    this.#commentMap = new Map();
-    this.#commentList = [];
-    this.#typingQueue = [];
-
-    oneComments.forEach((oneComment) => {
-      const id = oneComment.data.id;
-      const lorComment = oldMap.get(id) ?? new LorAnimationComment(oneComment);
-      lorComment.refreshContent();
-      this.#commentMap.set(id, lorComment);
-      this.#commentList.push(lorComment);
-      this.#typingQueue.push(...lorComment.characters.filter((c) => !c.hasActivated));
-    });
-
-    this.#startTypingAnimation();
-  }
-
-  /**
-   * タイピングアニメーションを開始します。
-   */
-  #startTypingAnimation() {
-    if (this.#typingTimerId != null) {
-      return;
-    }
-
-    this.#typingTimerId = setInterval(() => {
-      if (this.#typingQueue.length == 0) {
-        this.#stopTypingAnimation();
-        return;
-      }
-
-      const char = this.#typingQueue.shift();
-      if (char == null) {
-        this.#stopTypingAnimation();
-        return;
-      }
-      
-      char.activate();
-    }, DELAY_MILLISECONDS);
-  }
-
-  /**
-   * タイピングアニメーションを停止します。
-   */
-  #stopTypingAnimation() {
-    if (this.#typingTimerId == null) {
-      return;
-    }
-
-    clearInterval(this.#typingTimerId);
-    this.#typingTimerId = null;
-  }
-}
 
 /** @type {LorAnimator} LoR 風アニメーションの制御オブジェクト */
 const animator = new LorAnimator();
